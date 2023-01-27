49adf8b1cccc9b7a10b75604c30b6a7243b1262e
maruwan211 <75263869+maruwan211@users.noreply.github.com>
2023-01-27 09:52:55 +0700

    db
    database

---
+++ id20139699_db_student.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 26, 2023 at 04:13 PM
+-- Server version: 10.5.16-MariaDB
+-- PHP Version: 7.3.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id20139699_db_student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `a_id` int(10) NOT NULL,
+  `a_username` varchar(45) NOT NULL,
+  `a_pass` varchar(45) NOT NULL,
+  `a_level` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`a_id`, `a_username`, `a_pass`, `a_level`) VALUES
+(1, 'admin', '1234', 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customers`
+--
+
+CREATE TABLE `tbl_customers` (
+  `c_no` int(11) NOT NULL,
+  `S_Name` varchar(100) NOT NULL,
+  `S_LastName` varchar(100) NOT NULL,
+  `S_Address` varchar(100) NOT NULL,
+  `S_SunjectName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_customers`
+--
+
+INSERT INTO `tbl_customers` (`c_no`, `S_Name`, `S_LastName`, `S_Address`, `S_SunjectName`) VALUES
+(18, 'ทดสอบ1', 'เข้าระบบ1', 'ยะลา1', 'คอมพิวเตอร์1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product_type`
+--
+
+CREATE TABLE `tbl_product_type` (
+  `t_id` int(10) NOT NULL,
+  `t_name` varchar(45) NOT NULL,
+  `t_detail` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_student`
+--
+
+CREATE TABLE `tbl_student` (
+  `id_student` int(11) NOT NULL,
+  `s_name` varchar(45) DEFAULT NULL,
+  `s_lastname` varchar(45) DEFAULT NULL,
+  `s_address` varchar(255) DEFAULT NULL,
+  `s_subject` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_student`
+--
+
+INSERT INTO `tbl_student` (`id_student`, `s_name`, `s_lastname`, `s_address`, `s_subject`) VALUES
+(2, 'MARUWAN2222', 'MA-MU', 'yala2 update', 'account2'),
+(3, 'TAMANOON', 'TAMANOON', 'yala3 date', 'businesscomputer3');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`a_id`);
+
+--
+-- Indexes for table `tbl_customers`
+--
+ALTER TABLE `tbl_customers`
+  ADD PRIMARY KEY (`c_no`);
+
+--
+-- Indexes for table `tbl_product_type`
+--
+ALTER TABLE `tbl_product_type`
+  ADD PRIMARY KEY (`t_id`);
+
+--
+-- Indexes for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`id_student`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `a_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_customers`
+--
+ALTER TABLE `tbl_customers`
+  MODIFY `c_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `tbl_product_type`
+--
+ALTER TABLE `tbl_product_type`
+  MODIFY `t_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  MODIFY `id_student` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
